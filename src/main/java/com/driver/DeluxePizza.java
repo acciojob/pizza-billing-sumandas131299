6b7298896f0b8cca6757151733d0a0e767ee9098
Suman Das <sumandas131299@gmail.com>
2023-09-16 00:08:59 +0530

    final commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,33 +2,9 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    @Override
-    public int getPrice() {
-        return super.getPrice();
-    }
-
-    @Override
-    public void addExtraCheese() {
-
-    }
-
-    @Override
-    public void addExtraToppings() {
-
-    }
-
-    @Override
-    public void addTakeaway() {
-        super.addTakeaway();
-    }
-
-    @Override
-    public String getBill() {
-        return super.getBill();
-    }
-
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        addExtraToppings();
+        addExtraCheese();
     }
 }
